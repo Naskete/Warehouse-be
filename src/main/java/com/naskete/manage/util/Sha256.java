2b0cb7f771b2acd841203679d82b6e4e34b5fbdd
Naskete <1776795871@qq.com>
2022-07-11 18:26:16 +0800

    :sparkles:sha256 encode

---
+++ src/main/java/com/naskete/manage/util/Sha256.java
@@ -0,0 +1,39 @@
+package com.naskete.manage.util;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.CryptoPrimitive;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Sha256 {
+    private static String sha(String str) {
+        String res = null;
+        MessageDigest messageDigest;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(str.getBytes(StandardCharsets.UTF_8));
+            byte byteBuffer[] = messageDigest.digest();
+            StringBuffer hexString = new StringBuffer();
+            // 将byte转换为string
+            for (byte b : byteBuffer) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            res = hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+    public static String encode(String password) {
+        return sha(password);
+    }
+
+    public static boolean check(String password, String sha256) {
+        return encode(password).equals(sha256);
+    }
+}
