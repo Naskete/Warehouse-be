a5e6eadb3ffe27613aa08e3a22af2ad5c51f4af1
Naskete <1776795871@qq.com>
2022-08-09 18:46:13 +0800

    add getter and setter

--- src/main/java/com/naskete/manage/util/ResultJson.java
+++ src/main/java/com/naskete/manage/util/ResultJson.java
@@ -22,4 +22,28 @@ public class ResultJson {
         this.message = message;
         this.data = data;
     }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
